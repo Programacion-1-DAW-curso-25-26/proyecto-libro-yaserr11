8c29a8111b2a47d35f4dd922d92ba27980ccc06f
yassir <yaserr11@alu.edu.gva.es>
2026-01-20 09:30:59 +0100

    Arratlist comprobar equals

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class Main {
 
     public void main(String[] args) {
@@ -9,44 +12,59 @@ public class Main {
         michiavelli.setValoracion(5);
         michiavelli.setIsbn(10);
         michiavelli.mostrarInfo();
-        Libro dada = new Libro("DADO", "papa", 50, 10, 1);
-        Libro dado = new Libro("DADO", "papa", 50, 10, 9);
+        Libro libro1 = new Libro("DADO", "papa", 50, 10, 9);
+        Libro libro2 = new Libro("DADO", "papa", 50, 10, 9);
 
-        if (dado.equals(dada)) System.out.println("dada");
+        if (libro2.equals(libro1)) System.out.println("libro1");
         else System.out.println("no va");
         System.out.println(michiavelli);
 
-        Cancion popstar = new Cancion();
-        popstar.setAutor("shaw");
-        popstar.setTitulo("popstar");
-        popstar.setDuracion(-5);
-        popstar.imprimirInfo();
+        ArrayList <Libro> biblioteca = new ArrayList<>();
+        biblioteca.add(libro1);
+
+        System.out.println(biblioteca.contains(libro2));
+        // true, por que hay un libro en la bibiliot con el mismo isbn contain=equals
+
+        HashSet<Libro> casera = new HashSet<>();
+        System.out.println(casera.add(libro1));
+        System.out.println(casera.add(libro2));
+        System.out.println(casera.size());
+
+//        for (Libro l: biblioteca){
+//            System.out.println(l);
+//        }
+
+//        Cancion popstar = new Cancion();
+//        popstar.setAutor("shaw");
+//        popstar.setTitulo("popstar");
+//        popstar.setDuracion(-5);
+//        popstar.imprimirInfo();
 
 
 
-
-
-        Alumno Juan = new Alumno();
-        Juan.Alumno("Juan",16,"2 DAM");
-        Alumno Maria = new Alumno();
-        Maria.Alumno("Maria", 15);
-        Juan.mostrarDatos();
-        Maria.mostrarDatos();
-
-        CuentaBancaria Yassir = new CuentaBancaria();
-//        Yassir.setTitular("Yassir");
-        Yassir.ingresar(1000);
-        Yassir.retirar(2000);
-        Yassir.retirar(500);
-        Yassir.mostrarInfo();
-
-        System.out.println("total de bicicletas " +Bicicleta.getBicicletasTotales());
-        Bicicleta Orbea = new Bicicleta();
-        Orbea.nuevaBicicleta("Orbea","BMX");
-        Bicicleta Trek = new Bicicleta();
-        Trek.nuevaBicicleta("Trek","Trek");
-        Bicicleta Specialized = new Bicicleta();
-        Specialized.nuevaBicicleta("Specialized","Trek");
-        System.out.println("total de bicicletas " +Bicicleta.getBicicletasTotales());
+//
+//
+//        Alumno Juan = new Alumno();
+//        Juan.Alumno("Juan",16,"2 DAM");
+//        Alumno Maria = new Alumno();
+//        Maria.Alumno("Maria", 15);
+//        Juan.mostrarDatos();
+//        Maria.mostrarDatos();
+//
+//        CuentaBancaria Yassir = new CuentaBancaria();
+////        Yassir.setTitular("Yassir");
+//        Yassir.ingresar(1000);
+//        Yassir.retirar(2000);
+//        Yassir.retirar(500);
+//        Yassir.mostrarInfo();
+//
+//        System.out.println("total de bicicletas " +Bicicleta.getBicicletasTotales());
+//        Bicicleta Orbea = new Bicicleta();
+//        Orbea.nuevaBicicleta("Orbea","BMX");
+//        Bicicleta Trek = new Bicicleta();
+//        Trek.nuevaBicicleta("Trek","Trek");
+//        Bicicleta Specialized = new Bicicleta();
+//        Specialized.nuevaBicicleta("Specialized","Trek");
+//        System.out.println("total de bicicletas " +Bicicleta.getBicicletasTotales());
     }
 }
